6150763afcb11dd0e72089ca820a4c78a510450c
Luan Carrieiros <110491889+LuanCarrieiros@users.noreply.github.com>
2025-12-01 22:55:02 -0300

    Create IniciarServidor.bat

---
+++ IniciarServidor.bat
@@ -0,0 +1,9 @@
+@echo off
+echo ========================================
+echo   INICIANDO SERVIDOR OSINT
+echo   Porta TCP: 6789
+echo   Porta UDP: 9876
+echo ========================================
+echo.
+java ServidorOSINT
+pause
